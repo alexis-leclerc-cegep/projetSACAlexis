1127cd17b0424f4a49229464b08a23b48cdca1eb
= <=>
2022-11-03 17:28:49 -0400

    ajout du MyServer

---
+++ src/MyServer.cpp
@@ -0,0 +1,42 @@
+
+/**
+    Gestion d'un serveur WEB
+    @file MyServer.cpp
+    @author Alain Dubé
+    @version 1.1 20/11/20 
+*/
+#include <Arduino.h>
+#include "MyServer.h"
+using namespace std;
+
+typedef std::string (*CallbackType)(std::string);
+CallbackType MyServer::ptrToCallBackFunction = NULL;
+
+//Exemple pour appeler une fonction CallBack
+//if (ptrToCallBackFunction) (*ptrToCallBackFunction)(stringToSend); 
+void MyServer::initCallback(CallbackType callback) {
+    ptrToCallBackFunction = callback;
+    }
+
+void MyServer::initAllRoutes() { 
+    currentTemperature = 3.3f;
+
+    //Initialisation du SPIFF.
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+        }
+
+    //Route initiale (page html)
+    this->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+        });
+
+    //Route du script JavaScript
+    this->on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/script.js", "text/javascript");
+        });
+
+    
+    this->begin();
+};
