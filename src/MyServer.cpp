ee45ec5b261490d94199c3082921563e178070a4
= <=>
2022-11-17 17:33:28 -0500

    troubleshooting appel api

--- src/MyServer.cpp
+++ src/MyServer.cpp
@@ -43,17 +43,32 @@ this->on("/getAllWoodOptions", HTTP_GET, [](AsyncWebServerRequest *request) {
 
         HTTPClient http;
         String woodApiRestAddress = "http://51.79.84.135:9999/api/woodlist";
-        http.begin(woodApiRestAddress);
-        http.setAuthorization("authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsYSIsImlhdCI6MTY2MTUxNjAzNX0.4eGrq5XuJM1AcFnrxsRLHZp7e7E6v_u8lh6gEVAoU0Q");
-        http.GET();
-        String response = http.getString();
+        bool beginResult = http.begin(woodApiRestAddress);
+        if(!beginResult){
+            Serial.println("Erreur de connection au serveur");
+        }
+        else{
+            Serial.println("Connection au serveur réussie");
+            http.addHeader("authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsYSIsImlhdCI6MTY2MTUxNjAzNX0.4eGrq5XuJM1AcFnrxsRLHZp7e7E6v_u8lh6gEVAoU0Q");
+            http.GET();
+            String response = http.getString();
+            Serial.println(response);
+            http.end();
+        }
 
+/*
+        if(response[response.length()-1]==']') response[response.length()-1] = ' ';
+        if(response[0]=='[') response[0] = ' ';
+
+        Serial.println(response);
         String tempToSend;
         StaticJsonDocument<2048> doc;
         deserializeJson(doc, response);
         JsonObject obj1 = doc.as<JsonObject>();
         std::string wood;
-        String  woodName;
+        String woodName;
+
+
       
         for (JsonPair kv1 : obj1) {
             wood = kv1.key().c_str();
@@ -73,7 +88,9 @@ this->on("/getAllWoodOptions", HTTP_GET, [](AsyncWebServerRequest *request) {
             //    }
             }
         
-        request->send(200, "text/plain", tempToSend);
+        */
+        //request->send(200, "text/plain", tempToSend);
+        request->send(200, "text/plain", "okgood");
     });
 
 
